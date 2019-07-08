867e3bfd1f97e44201062ce5881e23d5d42849c4
waready <elbarto119@hotmail.com>
2019-07-07 22:29:08 -0500

    vutifi-dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container grid-list-md>
       <v-layout wrap>
           <v-flex class="primary" xs12>
             <h1>12</h1>
@@ -85,6 +85,71 @@
 
          </v-flex>
        </v-layout>
+       <v-layout row wrap="">
+         <v-flex xs12 md6>
+            <v-card>
+              <v-img  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+              </v-img>
+              <v-card-title>
+                <h2>lorem ipsun nada mas por ahora no hay problema</h2>
+              </v-card-title>
+              <v-card-text>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam enim quis, rerum ipsa porro aliquam similique in veritatis odio ducimus illum aspernatur facilis blanditiis est eos animi. Quasi, perspiciatis et!</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="info" flat>
+                  Accion
+                </v-btn>
+              </v-card-actions>
+            </v-card> 
+         </v-flex>
+         <v-flex xs12 md6>
+            <v-card>
+              <v-img  src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                <v-container fill-height="">
+                  <v-layout row wrap>
+                    <v-flex>
+                      <span class="headline white--text">asdasdsads</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-img>
+              <v-card-title>
+                <h2>lorem ipsun nada mas por ahora no hay problema</h2>
+              </v-card-title>
+              <v-card-text>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam enim quis, rerum ipsa porro aliquam similique in veritatis odio ducimus illum aspernatur facilis blanditiis est eos animi. Quasi, perspiciatis et!</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="info" flat  @click="dialog=true">
+                  Accion
+                </v-btn>
+              </v-card-actions>
+            </v-card> 
+         </v-flex>
+       </v-layout>
+       <v-dialog
+         v-model="dialog"
+          width="500px"
+          persistent=""
+       >
+         <v-card>
+           <v-card-title>
+             este es el titulo
+           </v-card-title>
+           <v-card-text>
+             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed sint dignissimos obcaecati, illum similique eligendi earum unde? Explicabo nostrum blanditiis quae ex reprehenderit recusandae commodi accusamus dolor, quia exercitationem optio.
+
+           </v-card-text>
+           <v-divider/>
+             <v-card-actions>
+                <v-btn class="error" @click="dialog=false"> 
+                  Cerrar
+                </v-btn> 
+             </v-card-actions>
+           
+         </v-card>
+       </v-dialog>
   </v-container>
 </template>
 
@@ -92,6 +157,10 @@
   
 
   export default {
-    
+    data(){
+      return{
+        dialog:false
+      }
+    }
   }
 </script>
