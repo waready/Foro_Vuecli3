62b7db2bdeaa4f8faa9277f58a7f16cc152230b5
waready <elbarto119@hotmail.com>
2019-07-07 22:03:56 -0500

    vuetify - pruebas

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+   <v-toolbar app=""  class="warning">
+     <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+     <v-toolbar-title>
+        Vuetify
+     </v-toolbar-title>
+     <v-spacer></v-spacer>
+     <v-btn class="success">
+       Igreso
+     </v-btn>
+     <v-btn class="error">
+       Salir
+     </v-btn>
+   </v-toolbar>
+    <v-navigation-drawer app="" v-model="drawer" temporary="" dark>
+      <v-layout mt-4 column="" align-center=""> 
+        <v-flex>
+          <v-avatar>
+             <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-avatar>
+        </v-flex>
+        <v-flex class="white--text mt-3 headline">
+          Juanito Peres
+        </v-flex>
+      </v-layout>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      drawer:true
+    }
+  }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+      <v-layout wrap>
+          <v-flex class="primary" xs12>
+            <h1>12</h1>
+          </v-flex>
+          <v-flex class="secondary" xs6>
+            <h1>6</h1>
+          </v-flex>
+          <v-flex class="error" xs6>
+            <h1>6</h1>
+          </v-flex>
+      </v-layout>
+      <v-layout mt-5 class="secondary" justify-center align-center="" style="height:300px;">
+        <v-flex class="info" xs3>
+          <h1>Centrado</h1>
+        </v-flex>
+         <v-flex class="success" xs3>
+          <h1>Centrado</h1>
+        </v-flex>
+      </v-layout>
+       <v-layout row wrap>
+         <v-flex>
+           <v-btn color="primary">
+             Texto
+           </v-btn>
+           <v-btn>
+             text
+           </v-btn>
+           <v-btn color="blue" dark="">
+             Text
+           </v-btn>
+           <v-btn color="secondary" flat="">
+             sin - fondo 
+           </v-btn>
+           <v-btn color="warning" large>
+             LArgo
+           </v-btn>
+           <v-btn color="warning" small>
+             Small
+           </v-btn>
+             <v-btn color="warning" large disabled="">
+             LArgo
+           </v-btn>
+           <v-btn color="info" depressed="">
+             Boton
+           </v-btn>
+             <v-btn color="warning" large>
+              <v-icon>forum</v-icon>
+           </v-btn>
+           <v-btn color="primary" icon flat>
+              <v-icon>forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" fab>
+              <v-icon>forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" small fab="">
+              <v-icon>forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" >
+             texto
+              <v-icon>forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" >
+             texto
+              <v-icon class="ml-2">forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" >
+             texto
+              <v-icon right="">forum</v-icon>
+           </v-btn>
+           <v-btn color="warning" large>
+
+              <v-icon left="">forum</v-icon>
+              texto
+           </v-btn>
+           <v-btn color="red" outline="">
+             <v-icon left="">forum</v-icon>
+              texto
+           </v-btn>
+           <v-btn color="red" outline="" block="">
+             <v-icon left="">forum</v-icon>
+              texto
+           </v-btn>
+
+         </v-flex>
+       </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
